7954d574aa53cb9a2b5d278c7569e7ea69d289c6
Lukas Koller <lukaskoller6@gmail.com>
2018-02-23 17:27:11 +0100

    - added SearchBar

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'dart:convert';
 import 'dart:io';
 
 import 'package:flutter/material.dart';
+import 'package:flutter_search_bar/flutter_search_bar.dart';
 
 void main() => runApp(new MyApp());
 
@@ -29,58 +30,88 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  // client_id for the Unsplash Api
   String client_id =
       'ab7804f9f2d18cddb6cdace2ae5e957bdd9deec3dd16ce1280e7069be81f6d33';
   List<String> urls = [];
 
+  SearchBar searchBar;
+
   @override
   void initState() {
     super.initState();
-    String keyword = 'car';
-    requestImages(keyword);
+    // init SearchBar
+    searchBar = new SearchBar(
+        inBar: false,
+        setState: setState,
+        onSubmitted: onSubmitted,
+        buildDefaultAppBar: buildAppBar
+    );
   }
 
+  // get Image urls
   void requestImages(String keyword) {
+    // Search for image associated with the keyword
     String url =
         'https://api.unsplash.com/search/photos?query=$keyword&per_page=100';
 
     HttpClient httpClient = new HttpClient();
     httpClient.getUrl(Uri.parse(url)).then((request) {
-      // Optionally set up headers...
-      // Optionally write to the request object...
+      // pass the client_id in the header
       request.headers.add('Authorization', 'Client-ID $client_id');
       // Then call close.
       return request.close();
     }).then((response) {
       // Process the response.
       if (response.statusCode == 200) {
+        // response: OK
+        // decode JSON
         response.transform(UTF8.decoder).join().then((String json) {
           var data = JSON.decode(json);
+          // extract urls from the JSON
           List<String> urls =
               new List<String>.generate(data['results'].length, (index) {
             return data['results'][index]['urls']['small'];
           });
+          // set the State
           setState(() {
             this.urls = urls;
           });
         });
       } else {
+        // something went wrong :(
         print("Http error: ${response.statusCode}");
       }
     });
   }
 
+  // For the SearchBar
+  AppBar buildAppBar(BuildContext context) {
+    return new AppBar(
+        title: new Text('Unsplash Wallpapers'),
+        actions: [searchBar.getSearchAction(context)]);
+  }
+
+  // callback for the SearchBar
+  void onSubmitted(String keyword) {
+    // search for images associated to the keyword
+    requestImages(keyword);
+  }
+
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
-      appBar: new AppBar(
-        title: new Text(widget.title),
-      ),
+      // set SearchBar as AppBar
+      appBar: searchBar.build(context),
+      // GridView
       body: new GridView.count(
+        // 2 column
         crossAxisCount: 2,
+        // some padding
         padding: const EdgeInsets.all(10.0),
         mainAxisSpacing: 10.0,
         crossAxisSpacing: 10.0,
+        // init the individual ImageViews
         children: new List<Widget>.generate(urls.length, (index) {
           return new GridTile(
               child: new Card(
